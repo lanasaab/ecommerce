030c94176d6cea8aa866d52a536e663202611290
lanasaab <lanasaab520@gmail.com>
2024-06-19 02:48:53 -0700

    new

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import ImageContainer from './imageContainer';
 import { cardData } from '../constants/imageData';
+import { useQuery } from '@tanstack/react-query';
 
 const Main = () => {
+
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ['productsalll'],
+    queryFn: () => fetch("http://localhost:3000/products").then((res) =>
+      res.json()
+    )
+
+  })
+  if (isPending) return "pending"
+  if (error) return "error"
+
+
+  console.log(data)
+
+
+
   return (
     <main>
       <div className="max-w-6xl mx-auto px-4">
@@ -10,7 +28,7 @@ const Main = () => {
           <h2 className="text-3xl font-bold text-black text-center mb-8">Featured Products:</h2>
           <p className="text-xl italic text-black text-center mb-8">" Add a flavor to being a girl "</p>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            {cardData.map((item, index) => (
+            {data.map((item, index) => (
               <ImageContainer
                 key={index}
                 id={item.id}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
+
+// Create a client
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
   <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   </BrowserRouter>
 )
